344f9bf8e2d31ded5ce8d2f88140d0058eec4a2e
Alejandro <alejbautistaa@gmail.com>
2026-03-19 13:30:11 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from './assets/vite.svg'
+// import heroImg from './assets/hero.png'
+// import './App.css'
+
+function App() {
+  return (
+    <>
+        <div>
+          <h1>LocalUser</h1>
+          <p>Qué rollo</p>
+    </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import  Navbar from "../src/components/Navbar"
+function Navbar(){
+    return(
+        <>
+        <Navbar/>
+                <nav>
+            <ul>
+                <li>
+                    primate
+                </li>
+            </ul>
+        </nav>
+        </>
+
+    )
+}
+export default Navbar
